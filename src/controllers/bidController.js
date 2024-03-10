256a46b231a060894a40bd09d149a0d7ac66ab66
harsha-0110 <harshavardhan.a2020@vitstudent.ac.in>
2024-03-10 21:14:30 +0530

    update

--- src/controllers/bidController.js
+++ src/controllers/bidController.js
@@ -2,7 +2,6 @@ const Bid = require('../models/Bid');
 const Catch = require('../models/Catch');
 require('dotenv').config();
 
-// Example route to place a bid
 exports.placeBid = async (req, res) => {
     try {
         const { userId, bidAmount, catchId } = req.body;
@@ -21,22 +20,28 @@ exports.placeBid = async (req, res) => {
         if (new Date() > CatchDetail.endTime) {
             return res.status(400).json({ error: 'Bidding for this item has ended' });
         }
-        const updatedItem = await Catch.updateOne(
-            { _id: catchId },
-            { $set: { currentBid: bidAmount, highestBidder: userId } }
-        );
-  
-        // Create a new bid document
-        const newBid = new Bid({
-            userId,
-            bidAmount,
-            catchId
-        });
-    
-        // Save the bid to the database
-        await newBid.save();
-    
-        res.status(200).json({ message: 'Bid placed successfully', bid: newBid });
+
+        // Check if the user has already placed a bid
+        const existingBid = await Bid.findOne({ catchId: catchId, userId: userId });
+
+        if (existingBid) {
+            // If the user has already placed a bid, update the existing bid document
+            existingBid.bidAmount = bidAmount;
+            await existingBid.save();
+            return res.status(200).json({ message: 'Bid updated successfully', bid: existingBid });
+        } else {
+            // If the user has not placed a bid, create a new bid document
+            const newBid = new Bid({
+                userId,
+                bidAmount,
+                catchId
+            });
+        
+            // Save the bid to the database
+            await newBid.save();
+        
+            res.status(200).json({ message: 'Bid placed successfully', bid: newBid });
+        }
     } catch (error) {
         console.error('Error placing bid:', error);
         res.status(500).json({ error: 'Failed to place bid' });
