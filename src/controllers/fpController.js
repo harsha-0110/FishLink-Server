7cb1b10725a7d2b0cae1878224b91e17d425c32d
harsha-0110 <harshavardhan.a2020@vitstudent.ac.in>
2024-03-12 16:32:26 +0530

    update package

--- src/controllers/fpController.js
+++ src/controllers/fpController.js
@@ -1,6 +1,5 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const express = require('express');
-const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
