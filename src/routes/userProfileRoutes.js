443db5b29daf5b1c2ccd88af817a699b341ee1df
kushagra <ramjindal1404@gmail.com>
2024-04-29 17:22:09 +0530

    comments

--- src/routes/userProfileRoutes.js
+++ src/routes/userProfileRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const userProfileController = require('../controllers/userProfileController');
 
-// Route to fetch user profile
+// Route to fetch seller profile
 router.get('/userProfile/seller/:sellerId', userProfileController.getUserProfile); // Use :sellerId as a parameter in the URL
 
 
